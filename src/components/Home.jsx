1ed8ff6ebac9f64b5ed7eb4795e2a70746131a6d
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-03-26 23:29:32 +0530

    4th

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
       <div className=' h-screen bg-black flex items-center justify-center ' >
         <div className=' flex text-white'>
           <div className=''>
-            <div className=' text-4xl ml-6 md:text-8xl sm:text-5xl font-[700] ' >Lazim Hostel</div>
-            <div className=' md:ml-32 ml-6 mt-3 text-xl'> "There is only one boss. the Guest."</div>
+            <div className=' text-[50px] ml-5 md:text-8xl sm:text-5xl font-[700]  inline-block' >Lazim Hostel</div>
+            <div className=' md:ml-32  ml-12 sm:ml-6 sm:mt-3 texl-lg sm:text-xl'> "There is only one boss. the Guest."</div>
             <div className='   mt-6 text-2xl flex justify-center'>
               <a className=' px-3' href="https://api.whatsapp.com/message/O247N4BWIKF3F1?autoload=1&app_absent=0">
                 <AiOutlineWhatsApp />
