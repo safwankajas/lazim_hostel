f3143bb30b108d8c5f8c732ce0917b4ea0a5cd57
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-05-31 15:27:45 +0530

    initial remove marker

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
             <nav className=' fixed  h-[10vh] bg-black  w-full '>
                 <div className='text-white  text-2xl flex justify-between p-3 '>
                     <div className=' text-white  text-2xl '>Lazim</div>
-                    <div className='  text-white'>X</div>
+                    {/* <div className='  text-white'>X</div> */}
                 </div>
             </nav>
         </div>
